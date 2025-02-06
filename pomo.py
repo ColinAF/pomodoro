a6ecb4d2430db0fc4c52a87ec584835edfdcb77d
Colin Frisch <cafrisch98@gmail.com>
2025-02-05 20:34:49 -0800

    Added notes

--- pomo.py
+++ pomo.py
@@ -1,11 +1,25 @@
 import time
 import curses
 
+# TODO
+# Center Text
+# Add start / stop
+# Display a cool session counter
+# Add cool ascii pretty print 
+# Print a nice bit of art and a cool outline
+# Add in Intentions
+# Add in a ding
+# Log Session Stats
+# Optional notifications
+# Text file for settings
+
 def pomodoro_timer(stdscr, duration):
     stdscr.clear()
     curses.curs_set(0)
 
+	# Fix duration glitch
     while duration:
+		# More accurate timer? 
         mins, secs = divmod(duration, 60)
         timer = f'{mins:02d}:{secs:02d}'
         stdscr.addstr(0, 0, "Pomodoro Timer")
