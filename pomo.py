33d35db8f1599865614a7f09fcf094a70cea96c5
Colin Frisch <cafrisch98@gmail.com>
2025-02-11 23:04:53 -0800

    Changed ding to 16-bit PCM, code now plays the sound but segfaults

--- pomo.py
+++ pomo.py
@@ -1,5 +1,7 @@
 import time
 import curses
+import threading
+import simpleaudio as sa
 
 # TODO
 # Increase font size
@@ -15,11 +17,26 @@ import curses
 
 TIMES_UP = "Time's up!"
 
+def play_sound():
+    # The wave file must be in 16-bit PCM format
+    wave_obj = sa.WaveObject.from_wave_file("ding.wav")
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+
 def pomodoro_timer(stdscr, duration):
+    # Start the playback in a daemon thread (it'll exit with the main program)
+    sound_thread = threading.Thread(target=play_sound, daemon=True)
+    sound_thread.start() 
+   
+    # Start the playback in its own thread
+    #sound_thread = threading.Thread(target=play_sound)
+    #sound_thread.start()
+    #sound_thread.join()  # Wait for the sound to finish before proceeding
+    
     stdscr.clear()
     curses.curs_set(0)
-
-	# Fix duration glitch
+   
+    # Fix duration glitch
     while duration:
 		# More accurate timer? 
         mins, secs = divmod(duration, 60)
